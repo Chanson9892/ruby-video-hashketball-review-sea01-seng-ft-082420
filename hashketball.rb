99c1483fbd3cd3fbe60b4f4d9da2646c68c84d9d
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-14 01:12:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,7 @@ end
 
 # Write code here
 def players
-  players = game_hash[:home][:players].merge(game_hash[:away][:players])
+  game_hash[:home][:players].merge(game_hash[:away][:players])
 end
 
 def teams(team_name)
